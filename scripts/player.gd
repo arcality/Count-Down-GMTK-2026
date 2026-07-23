5284cec900a93f784583530e1bb2269867630e05
Spiider <spiiderdev80@gmail.com>
2026-07-23 12:57:46 -0500

    resized everything

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,8 @@ class_name Player extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const ACCELERATION = 80
-const DECELERATION = 20
+const ACCELERATION = 16
+const DECELERATION = 4
 
 
 func _physics_process(delta: float) -> void:
